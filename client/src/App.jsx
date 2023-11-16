2305869ac3bd6bc248859d917707ceabdc56e7d5
bkisia <137644764+bkisia@users.noreply.github.com>
2023-11-16 14:44:43 +0100

    updated

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import Weather from './Components/Weather';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
